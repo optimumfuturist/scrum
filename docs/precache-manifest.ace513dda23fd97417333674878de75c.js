2fae7624d6aa2fdabefb229c0a25abc021fcb5d6
shekhardesigner <itsmes.sh@gmail.com>
2020-09-04 18:00:20 +0545

    Less animation, removed starter animation

---
+++ docs/precache-manifest.ace513dda23fd97417333674878de75c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0285b61bbba96efd6df7fa4724d4d139",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8e71431e06e064553e8",
+    "url": "./static/css/2.34627171.chunk.css"
+  },
+  {
+    "revision": "8b38550cf3538d95fea8",
+    "url": "./static/css/main.153aef76.chunk.css"
+  },
+  {
+    "revision": "a8e71431e06e064553e8",
+    "url": "./static/js/2.737371dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.737371dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8b38550cf3538d95fea8",
+    "url": "./static/js/main.97e81a89.chunk.js"
+  },
+  {
+    "revision": "4178712acadd5f8e7c35",
+    "url": "./static/js/runtime-main.c58ca154.js"
+  }
+]);
