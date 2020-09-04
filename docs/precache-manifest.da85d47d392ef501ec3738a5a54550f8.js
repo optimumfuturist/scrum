9c33503c0ee843761f2ecb06d08d95acc4937f22
shekhardesigner <itsmes.sh@gmail.com>
2020-09-04 15:51:12 +0545

    new base build

---
+++ docs/precache-manifest.da85d47d392ef501ec3738a5a54550f8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4c1b0282387c350647e9ec63d7f8a0d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a8e71431e06e064553e8",
+    "url": "./static/css/2.34627171.chunk.css"
+  },
+  {
+    "revision": "201e09b2212aa885a70a",
+    "url": "./static/css/main.153aef76.chunk.css"
+  },
+  {
+    "revision": "a8e71431e06e064553e8",
+    "url": "./static/js/2.737371dc.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "./static/js/2.737371dc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "201e09b2212aa885a70a",
+    "url": "./static/js/main.0d4dd8a6.chunk.js"
+  },
+  {
+    "revision": "4178712acadd5f8e7c35",
+    "url": "./static/js/runtime-main.c58ca154.js"
+  }
+]);
